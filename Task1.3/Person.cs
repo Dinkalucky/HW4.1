2b6ee9f903332c0965f5c8c3d0d6365243a978c0
Dina Nevhod <Dina_Nevhod@epam.com>
2023-03-31 17:58:33 +0200

    Task1.3

--- Task1.3/Person.cs
+++ Task1.3/Person.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,5 +18,73 @@ namespace Task1._3
 
         public string Name { get; set; }
         public int Year { get; set; }
+        public List<Person> Children { get; set; } = new List<Person>();
+
+        public void ShowFamily(Person person, string name)
+        {
+            ShowFamily(person, 0, name);
+        }
+
+        private void ShowFamily(Person person, int level, string name)
+        {
+            foreach (var f in person.Children)
+            {
+                if (f.Name == name)
+                {
+                    foreach (var child in f.Children)
+                    {
+                        ShowFamily(child, level + 1);
+                    }
+                }
+            }
+        }
+
+        public void ShowFamily(Person person, int level)
+        {
+            Console.WriteLine("".PadLeft(level * 4) + (person.Children.Any() ? " + " : " - ") + person.Name + "-" +
+                              person.Year);
+            foreach (var child in person.Children)
+            {
+                ShowFamily(child, level + 1);
+            }
+        }
+
+        public void ShowRelatives(Person person, int year)
+        {
+            if (person.Year > year)
+            {
+                Console.WriteLine(person.Name + "-" + person.Year);
+            }
+
+            foreach (var f in person.Children)
+            {
+                ShowRelatives(f, year);
+            }
+        }
+
+        public void AddChild(Person person, string parentName, string name, int year)
+        {
+            foreach (var f in person.Children)
+            {
+                AddChild(f, parentName, name, year);
+            }
+
+            if (person.Name == parentName)
+            {
+                person.Children.Add(new Person(Name = name, Year = year));
+            }
+        }
+
+        public void RemoveChild(Person person, string name)
+        {
+            foreach (var f in person.Children)
+            {
+                if (f.Name == name)
+                {
+                    person.Children.Remove(f);
+                    break;
+                }
+            }
+        }
     }
 }
--- Task1.3/Program.cs
+++ Task1.3/Program.cs
@@ -3,39 +3,102 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Task1._3
 {
-    internal class Program<T>
+    public class Program
     {
         static void Main(string[] args)
         {
-            var firstGeneration = new List<Person>
+            Console.OutputEncoding = Encoding.UTF8;
+            
+            var Family = new Person("Дідусь Володимир", 1910)
             {
-                new Person("Діна",1996),
-                new Person("Юлія",1984)
+                Children = new List<Person>()
+                {
+                    new Person("Тітка Катерина", 1963)
+                    {
+                        Children = new List<Person>()
+                        {
+                            new Person("Кузен Сергій", 1980),
+                            new Person("Кузен Олександр", 1986)
+                            {
+                                Children = new List<Person>()
+                                {
+                                    new Person("Племінник Вячеслав", 2012),
+                                    new Person("Племінниця Емілія", 2017),
+                                }
+                            },
+                            new Person("Кузена Маша", 2001),
+                        }
+                    },
+                    new Person("Тітка Наташа", 1967)
+                    {
+                        Children = new List<Person>()
+                        {
+                            new Person("Кузена Світлана", 1979),
+                            new Person("Кузена Марія", 1986),
+                            new Person("Кузен Федір", 1989),
+                        }
+                    },
+                    new Person("Тітка Галя", 1955)
+                    {
+                        Children = new List<Person>()
+                        {
+                            new Person("Кузен Олексій", 1980)
+                            {
+                                Children = new List<Person>()
+                                {
+                                    new Person("Племінниця Настя", 2003),
+                                    new Person("Племінник Семен", 2011),
+                                }
+                            }
+                        }
+                    },
+                    new Person("Мама", 1960)
+                    {
+                        Children = new List<Person>()
+                        {
+                            new Person("Сестра Юля", 1984)
+                            {
+                                Children = new List<Person>()
+                                {
+                                    new Person("Племінниця Аріна", 2014),
+                                }
+                            },
+                            new Person ("Діна", 1996)
+                        }
+                    },
+                }
             };
 
-            var secondGeneration = new List<Person>
-            {
-                new Person("Світлана",1996),
-                new Person("Анатолій",1984)
-            };
+            Console.WriteLine("Введіть ім'я родича, щоб побачити його нащадків");
+            string name = Console.ReadLine();
 
-            var grandParentsM = new List<Person>
-            {
-                new Person("Володимир",1927),
-                new Person("Марія",1930)
-            };
-            var grandParentsP = new List<Person>
-            {
-                new Person("Мефодій",1927),
-                new Person("Марія",1930)
-            };
-            var familyTree = new List<List<Person>>
-            {
-                new List<Person> { new Person("Володимир",1927), new Person("Володимир", 1927) }
-            };
+            Family.ShowFamily(Family, name);
+            Console.WriteLine("Повне родинне дерево:");
+            Family.ShowFamily(Family,0);
+
+            int year;
+            Console.WriteLine("Вивести усіх, хто молодше цього року народження");
+            Int32.TryParse(Console.ReadLine(), out year);
+
+
+            Family.ShowRelatives(Family, year);
+
+            Family.AddChild(Family,"Сестра Юля", "Діна2",1995);
+            Family.Name = "Дідусь Володимир";
+            Family.Year = 1910;
+            Console.WriteLine("Повне родинне дерево:");
+            Family.ShowFamily(Family, 0);
+
+            
+            Family.RemoveChild(Family, "Тітка Катерина");
+            Console.WriteLine("Повне родинне дерево:");
+            Family.ShowFamily(Family, 0);
+
+            Console.ReadLine();
         }
     }
 }
